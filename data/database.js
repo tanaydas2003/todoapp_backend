2fea1bbb40bc5ab8ba5085c92b015604b17d45d7
tanaydas069 <tane.das135@gmail.com>
2023-11-11 09:49:05 +0530

    update commit

--- data/database.js
+++ data/database.js
@@ -1,11 +1,25 @@
+// const mongoose = require("mongoose");
+
+// const connectDB = () =>{
+//     mongoose.connect("mongodb+srv://nodeboy:yourPasswordHere@nodeboycluster0.ysgmfhi.mongodb.net/yourDatabaseNameHere?retryWrites=true&w=majority");
+
+// //     mongoose
+// //         .connect("mongodb+srv://nodeboy:nodeboy@cluster0.ysgmfhi.mongodb.net/?retryWrites=true",{
+// //         dbName:"backendapi",
+// // })
+//     .then((c) => console.log(`database connected with ${c.connection.host}`))
+//     .catch((e) => console.log(e));
+// };
+// module.exports = connectDB;
 const mongoose = require("mongoose");
 
-const connectDB = () =>{
-    mongoose
-        .connect("mongodb://localhost:27017",{
-        dbName:"backendapi",
-})
-    .then(() => console.log("database connected"))
-    .catch((e) => console.log(e));
+const connectDB = () => {
+    mongoose.connect("mongodb+srv://nodeboy:nodeboy@cluster0.zamv3wa.mongodb.net/backendapi?retryWrites=true", {
+        useNewUrlParser: true, // Add this option to use the new parser
+        useUnifiedTopology: true, // Add this option to use the new topology engine
+    })
+    .then((c) => console.log(`database connected with ${c.connection.host}`))
+    .catch((error) => console.error("Error connecting to database:", error));
 };
+
 module.exports = connectDB;
